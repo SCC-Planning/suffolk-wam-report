86abfc23d173f7896f862eb7bf364f9a5506d554
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2022-10-27 11:38:36 +0100

    Section on waste movements

--- R/masons_analysis.R
+++ R/masons_analysis.R
@@ -112,3 +112,93 @@ landfill_totals <- landfill_totals[order(-landfill_totals$tonnes),]
 
 scc_barchart_grouped(landfill_totals, landfill_totals$`Site Name`, landfill_totals$`tonnes`, landfill_totals$year , "Waste Received to Landfill in Suffolk (tonnes)") +
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
+
+# Waste movements----
+# Who do we import our waste from?----
+wdi_origin <- lapply(years_of_interest, function(year) {
+  wdi <- read_excel(here("Data", paste0(year," Waste Received.xlsx"))) %>%
+    filter(`Facility WPA` == "Suffolk")
+  
+  totals  <- wdi %>%
+    select(`Tonnes Received`, `Origin WPA`) %>%
+    group_by(`Origin WPA`) %>%
+    summarise(tonnes = sum(`Tonnes Received`))
+  
+  totals$year <- year
+  
+  return(totals)
+})
+
+relevant_counties <- c(
+  "Suffolk",
+  "Norfolk",
+  "Essex",
+  "Cambridgeshire",
+  "Peterborough"
+)
+
+wdi_origin <- bind_rows(wdi_origin)
+
+counties <- wdi_origin %>%
+  filter(`Origin WPA` %in% relevant_counties)
+
+other <- wdi_origin %>%
+  filter(!`Origin WPA` %in% relevant_counties) %>%
+  select(tonnes, year) %>%
+  group_by(year) %>%
+  summarise(tonnes = sum(tonnes))
+
+other$`Origin WPA` <- "Other"
+
+wdi_origin_table <- bind_rows(counties, other)
+
+wdi_origin_plot <- wdi_origin_table |> 
+  filter(`Origin WPA` != "Suffolk")
+
+wdi_origin_plot <- wdi_origin_plot[(order(-wdi_origin_plot$year)),]
+
+scc_barchart_grouped(wdi_origin_plot, wdi_origin_plot$`Origin WPA`, wdi_origin_plot$`tonnes`, wdi_origin_plot$year , "Waste Imported to Suffolk by Origin (tonnes)") +
+  theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
+
+wdi_origin_table <- wdi_origin_table %>%
+  pivot_wider(names_from = `year`, values_from = `tonnes`)
+
+wdi_origin_table <- wdi_origin_table[(order(-wdi_origin_table$`2021`)),]
+
+suffolk_total <- wdi_origin_table %>%
+  filter(`Origin WPA` == "Suffolk")
+
+from_others <- data.frame(
+  `Origin` = c("Total waste managed origin outwith Suffolk"),
+  `2019` = sum(wdi_origin_table$`2019`) - suffolk_total$`2019`,
+  `2020` = sum(wdi_origin_table$`2020`) - suffolk_total$`2020`,
+  `2021` = sum(wdi_origin_table$`2021`) - suffolk_total$`2021`
+)
+
+managed_totals <- data.frame(
+  `Origin` = c("Total waste managed in Suffolk"),
+  `2019` = sum(wdi_origin_table$`2019`),
+  `2020` = sum(wdi_origin_table$`2020`),
+  `2021` = sum(wdi_origin_table$`2021`)
+)
+colnames(from_others) <- colnames(wdi_origin_table)
+colnames(managed_totals) <- colnames(wdi_origin_table)
+
+wdi_origin_table <- bind_rows(wdi_origin_table, from_others)
+wdi_origin_table <- bind_rows(wdi_origin_table, managed_totals)
+
+wdi_origin_table$`2019` <- totals_formatting(wdi_origin_table$`2019`)
+wdi_origin_table$`2020` <- totals_formatting(wdi_origin_table$`2020`)
+wdi_origin_table$`2021` <- totals_formatting(wdi_origin_table$`2021`)
+
+wdi_origin_table %>%
+  flextable() %>%
+  set_table_properties(layout = "autofit") %>%
+  add_header_row(
+    colwidths = c(4),
+    values = c("All waste managed in Suffolk (tonnes)")
+  ) %>%
+  bold(i = 7:8, j = 1:4, part = "body") %>%
+  theme_vanilla()
+
+# Who do we export our waste to?----
