a8a0bd1b3e1c8f78e4639877342d11af9589e8ff
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2023-03-24 11:16:54 +0000

    Updates to lacw text

--- docs/R/read_lacw.R
+++ docs/R/read_lacw.R
@@ -5,7 +5,7 @@
 #' @param folder data folder with files.
 read_lacw <- function(folder) {
   lacw <- readODS::read_ods(
-    paste0(folder,"LA_and_Regional_Spreadsheet_2021_rev.ods"),
+    paste0(folder,"LA_and_Regional_Spreadsheet_202122.ods"),
     sheet = "Table_1",
     skip = 3
   )
--- docs/R/render_report_site.R
+++ docs/R/render_report_site.R
@@ -1,3 +1,5 @@
+lapply(paste0(here::here("R"), "/", list.files(here::here("R"))), source)
+
 folder <- paste0(here::here("Data"), "/")
 wdi_data <- read_wdi(folder)
 lacw_data <- read_lacw(folder)
